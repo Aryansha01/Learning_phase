aa94dce9dc0796469b86d87cc1065ecbb7b7e76e
Aryansha01 <ryansharma@Aryans-MacBook-Air.local>
2025-06-03 22:13:49 +0530

    Some more practices

---
+++ C++/Continue&break/Break.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<=5;i++){
+        cout<<"hii"<<" ";
+        cout<<"hey"<<" ";
+        break;
+        cout<<"how r u"<<" ";
+    }
+}
